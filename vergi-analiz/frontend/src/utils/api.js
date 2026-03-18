cd0ad780fb450a8ec592f2fc8c05b66e37554d91
uluca2015 <uluca2015@gmail.com>
2026-03-18 23:33:37 +0300

    api url düzeltildi

--- vergi-analiz/frontend/src/utils/api.js
+++ vergi-analiz/frontend/src/utils/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-    ? `https://${import.meta.env.VITE_API_URL}`
-    : 'http://localhost:8000',
+  baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
 })
 
